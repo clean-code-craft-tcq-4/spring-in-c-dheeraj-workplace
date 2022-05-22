a89bc39c2852e62d711e0d3c6fe07e653e4e7a27
dheeraj-workplace <105434414+dheeraj-workplace@users.noreply.github.com>
2022-05-22 19:34:38 +0530

    build issue resolved

--- notification.c
+++ notification.c
@@ -9,12 +9,3 @@ void ledAlerter(void)
 {
   ledAlertCallCount++;
 }
-
-void check_and_alert(const float threshold, alerter_funcptr alerter, struct Stats stat)
-{
-  if(stat.max > threshold)
-  {
-     (*alerter[0]);
-     (*alerter[1]);
-  }
-}
