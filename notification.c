534138003d2b7733c716a990cf4e44e09460baf6
dheeraj-workplace <105434414+dheeraj-workplace@users.noreply.github.com>
2022-05-22 19:08:37 +0530

    notification.c added for TEST3

---
+++ notification.c
@@ -0,0 +1,20 @@
+#include <notification.h>
+
+void emailAlerter(void)
+{
+  emailAlertCallCount++;
+}
+
+void ledAlerter(void)
+{
+  ledAlertCallCount++;
+}
+
+void check_and_alert(const float threshold, alerter_funcptr alerter, struct Stats stat)
+{
+  if(stat.max > threshold)
+  {
+     (*alerter[0]);
+     (*alerter[1]);
+  }
+}
