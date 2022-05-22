ad7db5ac7bc448834df5a8620133a11737436558
dheeraj-workplace <105434414+dheeraj-workplace@users.noreply.github.com>
2022-05-22 11:42:43 +0530

    compute_statistics updated for the first test case

--- stats.c
+++ stats.c
@@ -5,6 +5,41 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    if(setlength != 0)
+    {
+        if(setlength == 1)
+        {
+            s.average = numberset[0];
+            s.min = numberset[0];
+            s.max = numberset[0];
+        }
+        else
+        {
+            /* Finding the average, max and min value. */
+            s.min = numberset[0];
+            s.max = numberset[0];
+            s.average = numberset[0];
+            for(int index = 1; index < setlength; index++)
+            {
+                s.average += numberset[index];
+                
+                if(numberset[index] > s.max)
+                {
+                    s.max = numberset[index];
+                }
+                
+                if(numberset[index] < s.min)
+                {
+                    s.min = numberset[index];
+                }
+            }
+            s.average /= setlength;
+        }
+    }
+    
+    /* Returning the structure */
+    return s;
 }
 
 int emailAlertCallCount = 0;
